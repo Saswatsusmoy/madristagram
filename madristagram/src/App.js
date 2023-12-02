f4d290370ec309a0281e0daff2e948af2bb43ed9
arjxn.py <arjxn.py@gmail.com>
2023-12-02 19:06:54 +0530

    Add default route

--- madristagram/src/App.js
+++ madristagram/src/App.js
@@ -1,11 +1,16 @@
 import Home from "./Pages/Home/Home";
 import TopBar from "./Components/top-navbar/TopBar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <TopBar/>
-    <Home/>
+      <TopBar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
