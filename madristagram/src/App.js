a2ddcc3c1acbda63dd5245e29d96275d9f9e3521
arjxn.py <arjxn.py@gmail.com>
2023-12-02 19:11:59 +0530

    Fix signup import & Add login & signup routes

--- madristagram/src/App.js
+++ madristagram/src/App.js
@@ -1,5 +1,7 @@
 import Home from "./Pages/Home/Home";
 import TopBar from "./Components/top-navbar/TopBar";
+import LoginSignup from "./Components/Login_Signup/Login";
+import SignUp from "./Components/Login_Signup/Signup";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginSignup />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Router>
     </>
